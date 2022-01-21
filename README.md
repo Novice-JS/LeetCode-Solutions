f7c601f6e54528f1459522d1409aae651643fbf8
Kevin <kfig21@gmail.com>
2022-01-21 14:46:32 -0500

    adding solution to LC 2139 Minimum Moves to Reach Target Score

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # LeetCode-Solutions
 
-<h3>Easy</h3>
+<h3>[Easy]</h3>
 <ul>
   <li>1. Two Sum  |  Watch here <a href="https://www.youtube.com/watch?v=xfrMupld-eQ">📺</a>  |  LeetCode link <a href="https://leetcode.com/problems/two-sum/">🔗</a></li>
+</ul>
+
+<h3>[Medium]</h3>
+<ul>
+  <li>2139. Minimum Moves to Reach Target Score  |  Watch here <a href="https://www.youtube.com/watch?v=xfrMupld-eQ">📺</a>  |  LeetCode link <a href="https://leetcode.com/problems/minimum-moves-to-reach-target-score/">🔗</a></li>
 </ul>
---
+++ medium/2139-MinimumMovesToReachTargetScore.js
@@ -0,0 +1,36 @@
+var minMoves = function (target, maxDoubles) {
+  // create a variable to count our moves
+  let moves = 0;
+  // loop while target is greater than 1. once we reach 1 we will have our answer
+  while (target > 1) {
+    // if we have any max doubles...
+    if (maxDoubles > 0) {
+      // and if the target is even...
+      if (target % 2 === 0) {
+        // set the target to half of its current value
+        target /= 2;
+        // and decrease the max doubles by one
+        maxDoubles--;
+        // if the target is odd..
+      } else {
+        // decrease the target by 1
+        target--;
+      }
+      // after the conditional increase the moves counter by 1
+      moves++;
+      // if we don't have any maxDoubles...
+    } else {
+      // increase the moves by the value of target - 1
+      moves += target - 1;
+      // and set the target to 1 to short-circuit the loop
+      target = 1;
+    }
+  }
+  // return moves as our answer
+  return moves;
+};
+
+console.log(minMoves(5, 0)); // 4
+console.log(minMoves(19, 2)); // 7
+console.log(minMoves(10, 4)); // 4
+console.log(minMoves(999999999, 0)); // 999999998
